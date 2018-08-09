5dbb95549f58ba99302eabd08391f205eb6f745a
Kunwar Utkarsh Singh <nottherealutkarsh@gmail.com>
2018-08-09 10:02:21 +0530

    Initial Commit

---
+++ BullsEye 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye 1.0
+//
+//  Created by Alex Mercer on 09/08/18.
+//  Copyright © 2018 kunutkarsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
